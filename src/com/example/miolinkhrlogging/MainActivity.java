750f214511ae4f89dc95df8bafcf91fa7e3dbd38
brian8759 <brian8759@gmail.com>
2014-11-12 23:46:02 -0500

    adding basic blocks

--- src/com/example/miolinkhrlogging/MainActivity.java
+++ src/com/example/miolinkhrlogging/MainActivity.java
@@ -1,18 +1,61 @@
 package com.example.miolinkhrlogging;
 
+import java.util.ArrayList;
+
 import android.support.v7.app.ActionBarActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends ActionBarActivity {
 
+	private static final int REQUEST_ENABLE_BT = 1;
+	private BluetoothAdapter mBluetoothAdapter;
+	private ArrayList<BluetoothDevice> mDeviceList = new ArrayList<BluetoothDevice>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		if(mBluetoothAdapter == null) {
+			// Device does not support Bluetooth
+			
+		} else {
+			if(!mBluetoothAdapter.isEnabled()) {
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+				
+			}
+		}
+		IntentFilter filter = new IntentFilter();
+		
+		filter.addAction(BluetoothDevice.ACTION_FOUND);
+		
+		registerReceiver(mReceiver, filter);
 	}
 
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		@Override
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	        	// Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            mDeviceList.add(device);
+	        }
+	    }
+	};
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -30,5 +73,23 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	@Override
+	public void onPause() {
+		if (mBluetoothAdapter != null) {
+			if (mBluetoothAdapter.isDiscovering()) {
+				mBluetoothAdapter.cancelDiscovery();
+			}
+		}
+		
+		super.onPause();
+	}
+	
+	@Override
+	public void onDestroy() {
+		unregisterReceiver(mReceiver);
+		
+		super.onDestroy();
 	}
 }
